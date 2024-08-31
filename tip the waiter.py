b19ca382d80691af753cfcf4e7e699bf05d0fda7
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-08-31 17:34:04 +0600

    hello

--- tip the waiter.py
+++ tip the waiter.py
@@ -1,1 +1,12 @@
-print("hello world")
+def tipCalc(b, tipPerc):
+    tp = b * (0.01 * tipPerc)
+    return tp
+
+bill = int(input("Enter the bill:"))
+tip = int(input("Enter the percentage:"))
+
+total = bill + tipCalc(bill, tip)
+
+print(f"bill : {bill}")
+print(f"tip percentage : {tip}%")
+print(f"total bill : {total}")
